6ff575cb971e5d86a552828e09e29a31e810eb24
Connor Davis <cnnrproton@gmail.com>
2024-08-29 14:24:13 +0200

    Getting ready for transactional things

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,13 +1,4 @@
 import {
-  businessCollections as businessCollectionsSchema,
-  collectionsBusiness as collectionsBusinessesSchema,
-  collectionsCollector as collectionsCollectorsSchema,
-  collectionsProduct as collectionsProductsSchema,
-  collections as collectionsSchema,
-  collectorCollections as collectorCollectionsSchema,
-  productCollections as productCollectionsSchema,
-} from "./collections";
-import {
   businessProducts as businessProductsSchema,
   productsBusiness as productsBusinessesSchema,
   products as productsSchema,
@@ -18,8 +9,9 @@ import {
   userBusinesses as userBusinessesSchema,
 } from "./businesses";
 import {
-  collectionTransactions as collectionTransactionsSchema,
-  transactionsCollection as transactionsCollectionSchema,
+  businesssTransactions as businessTransactionsSchema,
+  transactionsBuyer as transactionsBuyerSchema,
+  transactionsSeller as transactionsSellerSchema,
   transactions as transactionsSchema,
 } from "./transactions";
 import {
@@ -48,14 +40,7 @@ export const products = productsSchema;
 export const productsBusinesses = productsBusinessesSchema;
 export const businessProducts = businessProductsSchema;
 
-export const collections = collectionsSchema;
-export const collectionsBusinesses = collectionsBusinessesSchema;
-export const collectionsCollectors = collectionsCollectorsSchema;
-export const collectionsProducts = collectionsProductsSchema;
-export const businessCollections = businessCollectionsSchema;
-export const collectorCollections = collectorCollectionsSchema;
-export const productCollections = productCollectionsSchema;
-
 export const transactions = transactionsSchema;
-export const transactionsCollection = transactionsCollectionSchema;
-export const collectionTransactions = collectionTransactionsSchema;
+export const transactionsBuyer = transactionsBuyerSchema;
+export const transactionsSeller = transactionsSellerSchema;
+export const businessTransactions = businessTransactionsSchema;
--- src/schemas/transactions.ts
+++ src/schemas/transactions.ts
@@ -1,13 +1,15 @@
 import { decimal, pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { relations, sql } from "drizzle-orm";
 
-import { collections } from "./collections";
+import { businesses } from "./businesses";
 
 export const transactions = pgTable("transactions", {
   id: uuid("id")
     .primaryKey()
     .default(sql`uuid_generate_v4()`),
-  collectionId: uuid("collection_id").notNull(),
+  buyerId: uuid("buyer_id").notNull(),
+  sellerId: uuid("seller_id").notNull(),
+  productId: uuid("product_id").notNull(),
   weight: decimal("weight").notNull(),
   createdAt: text("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
@@ -17,13 +19,20 @@ export const transactions = pgTable("transactions", {
     .notNull(),
 });
 
-export const transactionsCollection = relations(transactions, ({ one }) => ({
-  collection: one(collections, {
-    fields: [transactions.collectionId],
-    references: [collections.id],
+export const transactionsBuyer = relations(transactions, ({ one }) => ({
+  buyer: one(businesses, {
+    fields: [transactions.buyerId],
+    references: [businesses.id],
   }),
 }));
 
-export const collectionTransactions = relations(collections, ({ many }) => ({
+export const transactionsSeller = relations(transactions, ({ one }) => ({
+  buyer: one(businesses, {
+    fields: [transactions.buyerId],
+    references: [businesses.id],
+  }),
+}));
+
+export const businesssTransactions = relations(businesses, ({ many }) => ({
   transactions: many(transactions),
 }));

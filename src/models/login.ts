8197a7396c25728bd45c0fe2d5d3e863380cbaca
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-09-30 10:31:49 +0200

    Updates

--- src/models/login.ts
+++ src/models/login.ts
@@ -1,4 +1,12 @@
+import { UserRoles } from "../utilities/types";
+
 export type LoginModel = {
   email: string;
   password: string;
 };
+
+export type RegisterModel = {
+  email: string;
+  password: string;
+  role: UserRoles | undefined;
+}

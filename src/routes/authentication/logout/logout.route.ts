a4299024e2e6aa884c5db68645e0365049be8cd2
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-10-19 21:08:00 +0200

    Update logout route spec to have unauthorized message and middleware for authentication

--- src/routes/authentication/logout/logout.route.ts
+++ src/routes/authentication/logout/logout.route.ts
@@ -1,7 +1,11 @@
 import { createRoute, z } from "@hono/zod-openapi";
 
+import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
+
 import HttpStatus from "@/lib/http-status";
 import TAGS from "@/lib/tags";
+import authenticationMiddleware from "@/middleware/authentication-middleware";
 
 const logoutRoute = createRoute({
   path: "/authentication/logout",
@@ -11,12 +15,20 @@ const logoutRoute = createRoute({
     [HttpStatus.OK]: {
       content: {
         "text/plain": {
-          schema: z.string().default("The ok response text."),
+          schema: z.string().default("ok"),
         },
       },
       description: "The ok response text.",
     },
+    [HttpStatus.UNAUTHORIZED]: jsonContent(
+      createMessageObjectSchema(
+        "You are not authorized to access this endpoint"
+      ),
+      "The un-authorized error message."
+    ),
   },
+  middleware: async (context, next) =>
+    await authenticationMiddleware(undefined, context, next),
 });
 
 export type LogoutRoute = typeof logoutRoute;
